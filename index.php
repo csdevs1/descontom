b1bf39d665d871b2197da5bfed3008160fb0b153
Gabriel Pinango <gabrielpinango1492@gmail.com>
2015-07-31 19:04:29 -0430

    push to github

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <title>Descontom LDA</title>
+        <meta charset=utf-8 >
+        <link rel="stylesheet" href="css/stylesheet.css" />
+        <link rel="stylesheet" href="css/normalize.css" />
+        <link rel="stylesheet" href="css/component.css" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="descontomania" content="CCTV">
+        <meta name="descontom" content="descontomania">  
+        <meta name="Description" CONTENT="DESCONTOMANIA LDA. Fundada em 13 de fevereiro de 2014 é uma empresa dedicada à pesquisa, desenvolvimento e distribuição de equipamentos de vigilância e soluções">
+        <meta name="keywords" content="descontom, descontomania, cctv, analogo, ip, alarmes, nvrs, cameras, espião, liquidação, pesquisa, vigilância, dvr, ahd">
+        <meta name="fragment" content="!">
+        <meta property="og:title" content="Descontomania" />
+        <meta property="og:site_name" content="Descontomania">
+        <meta property="og:description" content="DESCONTOMANIA LDA. Fundada em 13 de fevereiro de 2014 é uma empresa dedicada à pesquisa, desenvolvimento e distribuição de equipamentos de vigilância e soluções." />
+        <meta property="og:image" content="https://i.imgur.com/jXSjZfk.png" />
+        <meta name="robots" content="follow,index">
+        <meta name="google-site-verification" content="k_qOMfGf5oFm6xyqlqPVPctFLlsxxWS_UAmaSl2o55o" />
+        <meta name="google-site-verification" content="gpAdbHz5FA7S0_vm74G8-o9dljPIBg0AQJyehbeSi_8" />
+        <script type="application/ld+json">
+            {
+              "@context": "https://descontom.herokuapp.com/",
+              "@type": "BreadcrumbList",
+              "itemListElement": [{
+                "@type": "ListItem",
+                "position": 1,
+                "item": {
+                  "@id": "https://descontom.herokuapp.com/#!/Alarmes/c/11134674",
+                  "name": "Alarmes"
+                }
+              },{
+                "@type": "BreadcrumbList",
+                "position": 2,
+                "item": {
+                  "@id": "https://descontom.herokuapp.com/#!/Analogo/c/10219130" title="analogo",
+                  "name": "Analogo"
+                }
+              },{
+                "@type": "BreadcrumbList",
+                "position": 3,
+                "item": {
+                  "@id": "https://descontom.herokuapp.com/#!/Ahd/c/13903037",
+                  "name": "Ahd"
+                }
+              }]
+            }
+        </script>
+        <script src="js/classie.js"></script>
+        <script src="js/gnmenu.js"></script>
+    </head>    
+    <body>
+        <div id="wrapper">
+            <header>
+                <div class="container">
+                    <ol id="gn-menu" class="gn-menu-main">
+                        <li class="gn-trigger">
+                            <a class="gn-icon gn-icon-menu"><span>Menu</span></a>
+                            <nav class="gn-menu-wrapper">
+                                <div class="gn-scroller">
+                                    <ol class="gn-menu">
+                                        <li>
+                                            <a class="gn-icon gn-icon-download"  href="#!/Alarmes/c/11134674" title="alarmes">Alarmes</a>
+                                            <ol class="gn-submenu">
+                                                <li><a class="gn-icon gn-icon-illustrator" href="#!/Acessorios/c/11938189">Accesorios</a></li>
+                                                <li><a class="gn-icon gn-icon-photoshop" #!/Alarmes/c/11938188>Alarmes</a></li>
+                                            </ol>
+                                        </li>
+                                        <li>
+                                            <a class="gn-icon gn-icon-download" href="#!/Analogo/c/10219130" title="analogo">Analogo</a>
+                                            <ol class="gn-submenu">
+                                                <li><a class="gn-icon gn-icon-illustrator" href="#!/Acessorios/c/13860136">Accesorios</a></li>
+                                                <li><a class="gn-icon gn-icon-illustrator" href="#!/Cameras/c/10219338">Camaras</a></li>
+                                                <li><a class="gn-icon gn-icon-photoshop" href="#!/Dvr/c/10219339">Dvr</a></li>
+                                            </ol>
+                                        </li>
+                                        <li>
+                                            <a class="gn-icon gn-icon-download" href="#!/Ahd/c/13903037" title="ahd">Ahd</a>
+                                            <ol class="gn-submenu">
+                                                <li><a class="gn-icon gn-icon-photoshop" href="#!/Dvr/c/13903040">Dvr</a></li>
+                                                <li><a class="gn-icon gn-icon-illustrator" href="#!/Cameras/c/13903039">Cameras</a></li>
+                                                <li><a class="gn-icon gn-icon-illustrator" href="#!/Acessorios/c/13903038">Accesorios</a></li>
+                                            </ol>
+                                        </li>
+                                        <li>
+                                            <a class="gn-icon gn-icon-download" href="#!/Ip/c/10219131" title="ip">Ip</a>
+                                            <ol class="gn-submenu">
+                                                <li><a class="gn-icon gn-icon-article" href="#!/Acessorios/c/13860137">Accesorios</a></li>
+                                                <li><a class="gn-icon gn-icon-pictures" href="#!/Kits/c/12841210">Kits</a></li>
+                                                <li><a class="gn-icon gn-icon-videos" href="#!/Nvrs/c/12841209">Nvrs</a></li>
+                                                <li><a class="gn-icon gn-icon-illustrator" href="#!/Cameras/c/12841207">Cameras</a></li>
+                                            </ol>
+                                        </li>
+                                        <li><a class="gn-icon gn-icon-article" href="#!/Cabos/c/10243180" title="cabos">Cabos</a></li>
+                                        <li><a class="gn-icon gn-icon-article" href="#!/Acessorios/c/10219337" title="accessorios">Accesorios</a></li>
+                                        <li><a class="gn-icon gn-icon-article" href="#!/Espião/c/12841216" title="espião">Espião</a></li>
+                                        <li><a class="gn-icon gn-icon-article" href="#!/Liquidação/c/13860121" title="liquidação">Liquidação</a></li>
+                                    </ol>
+                                </div><!-- /gn-scroller -->
+                            </nav>
+                        </li>
+                        <li><a href="/"><img src="img/logo.jpg" alt="Descontom" id="logo"></a></li>
+                        <li><a class="codrops-icon codrops-icon-prev" href="/"><span></span></a></li>
+                        <li>
+                            <span>
+                                <script type="text/javascript" src="https://app.ecwid.com/script.js?5236037" charset="utf-8"></script>
+                                <script type="text/javascript"> xSearchPanel("style="); </script>
+                            </span>
+                        </li>
+                    </ol>
+                </div><!-- /container --> 
+            </header>
+            <script>
+                new gnMenu( document.getElementById( 'gn-menu' ) );
+            </script>
+            <!-- ORIGINAL NAV 
+                <script src="http://app.ecwid.com/script.js?5236037"></script>
+                <div id="horizontal-menu"></div> 
+                <script src="https://djqizrxa6f10j.cloudfront.net/horizontal-category-widget/v1/horizontal-widget.js"></script>
+            -->
+
+            <div id="container">
+                <div id="my-store-5236037"></div>
+                <div>
+                    <script type="text/javascript" src="https://app.ecwid.com/script.js?5236037" charset="utf-8"></script><script type="text/javascript">
+                    xProductBrowser("categoriesPerRow=3","views=grid(3,3) list(10) table(20)","categoryView=grid","searchView=list","id=my-store-5236037");</script>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="footer-outer">
+                <ol id="ecwid-starter-site-links">
+                    <li>©
+                        2014–2015
+                        Descontom.com
+                    </li>
+                </ol>
+            </div>
+        </footer>
+    </body>
+</html>
